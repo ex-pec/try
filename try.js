67fdccca054ccbd2359d405328caef9595e717e1
ex-pec <65666637+ex-pec@users.noreply.github.com>
2020-06-21 21:49:56 +0300

    it's work

--- try.js
+++ try.js
@@ -1,78 +1,91 @@
 var subArrayFilteredIndex = []; //filtered sub array
 var maxSubArraySum = -99999999999; //for sum check
-var maxSubArrayIndis = 0; //biggest sum index
-var array = [
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19
-]; //19 eleman
+var maxSubArrayIndisArray = []; //biggest sum index
+var array = [-1, 9, 13, 10, -81, 69, 31, 199]; //20 eleman
+
+var realSubArray = []; //sub arrayler için buna gerek yok aslında indisler üzerinden çalışıyoruz
+
 var subArrayMaxLength = Math.ceil(array.length / 2); //10 eleman max
 var subArrayCompress = [];
-//array.shift();//baştaki elemanı çıkar
+
 for (let index = 0; index < array.length; index = index + 2)
   subArrayCompress.push(index);
 
+var tmpArrayChanged = array; //temp üzerinde işlem gerçekleştirilir
+var subArray;
 ///////////////////////////ham veri///////////////////////////
 
+//ana fonksiyon
 function fonk() {
   for (let index = 1; index <= subArrayMaxLength; index++) {
     var tmpSum = 0;
     if (index == 1) {
       //1 elemanlı tüm dizi indisleri arraye eklendi
       for (let index2 = 0; index2 < array.length; index2++) {
-        subArrayFilteredIndex.push(index2.toString()); //diziye ekle tek elemanlı sub strleri
-        tmpSum = index2; //toplam kontrolü için temp değer
-        if (tmpSum > maxSubArraySum) {
-          //daha büyük ise indis ve toplam güncellenir
-          maxSubArraySum = tmpSum;
-          maxSubArrayIndis = subArrayFilteredIndex.length - 1;
-        }
+        subArrayFilteredIndex.push(index2.toString()); //diziye ekle tek elemanlı sub str indislerini
+        const element = [];
+        element.push(index2.toString());
+        checkSumIndex(element, array);
       }
     } else {
       //son eleman gezme
-      for (let index2 = 0; index2 < array.length; index2++) {
-        lastMember(index);
-        
-      }
+      subArray = subArrayCompress.slice(0, index); //sıkışık bir dizi alınır
+      lastMember(index);
     }
   }
+  var str2 = array.toString();
+  realSubArray.forEach(element => {
+    str2 += "<br>" + element;
+  });
+  document.getElementById("add1").innerHTML = str2;
+  document.getElementById("add2").innerHTML = maxSubArraySum;
+  var str = "";
+  maxSubArrayIndisArray.forEach(element => {
+    str += realSubArray[element];
+  });
 
-  document.getElementById("add").innerHTML = subArrayFilteredIndex;
+  document.getElementById("add3").innerHTML = str;
+}
+
+////last member search
+function lastMember(index) {
+  //son eleman gezme
+
+  //forScroll değerini indisbelirle fonksiyonunu çağırdıktan sonra arttır
+  var forScroll = subArray[subArray.length - 2] + 2; //patlak sıkışık dizi dışında çalışmıyor
+
+  for (let index2 = forScroll; index2 < tmpArrayChanged.length - 1; index2++) {
+    //push gerçekleştir
+    subArrayFilteredIndex.push(subArray.toString()); //indis dizisi gerçek dizi değil
+    //subArray index dizisine at
+    checkSumIndex(subArray, tmpArrayChanged);
+    subArray[subArray.length - 1]++; //subArray son elemanı bir artırarak gez.
+  }
+  //son elemanı ekliyoruz
+  subArrayFilteredIndex.push(subArray.toString());
+  checkSumIndex(subArray, tmpArrayChanged);
+  
+  
+  while (!checkSubArrayEnd()) {
+    readyForShift();
+    lastMember(index);
+  }
 }
 
 //for indis search
-function indisBelirle(dizi, indis) {
-  //indis=3
-  var boyut = dizi.length;
-  //console.log(boyut)
-  for (let index = indis; index < boyut; index++) dizi[index]++;
-
-  return dizi;
+function indisBelirle(indis) {
+  subArray[indis]++;
+  for (; indis < subArray.length - 1; indis++)
+    subArray[indis + 1] = subArray[indis] + 2;
 }
-
 /////sum//////
 function checkSum(array, tmpArray) {
   var tmpSubArray = [];
-  for (let index = 0; index < array.length; index++) {
+  for (let index = 0; index < array.length; index++)
     tmpSubArray.push(tmpArray[array[index]]);
-  }
+
+  realSubArray.push(tmpSubArray.toString()); //gerçek verileri tutmak için dizi(indis değil)
+
   var sum = tmpSubArray.reduce(sumReduce);
 
   function sumReduce(total, value) {
@@ -87,30 +100,57 @@ function checkSumIndex(tmpSubArray, tmpArray) {
   if (tmpSum > maxSubArraySum) {
     //daha büyük ise indis ve toplam güncellenir
     maxSubArraySum = tmpSum;
-    maxSubArrayIndis = subArrayFilteredIndex.length - 1;
+    maxSubArrayIndisArray = []; //aynı değilse dizi temizlenir her seferinde
+    maxSubArrayIndisArray.push(subArrayFilteredIndex.length - 1); //yerini kaybetmemek için kayıt alıyoruz
+  } else if (tmpSum == maxSubArraySum) {
+    if (tmpSum > maxSubArrayIndisArray[maxSubArrayIndisArray.length - 1])
+      maxSubArrayIndisArray = [];
+
+    maxSubArrayIndisArray.push(subArrayFilteredIndex.length - 1);
   }
 }
-////last member search
-function lastMember(index) {
-  //son eleman gezme
-  var tmpArray = array;
-  var subArray = subArrayCompress.slice(0, index);
-  var tmpSubArray = subArray;
-  var temp = (tmpSubArray.length - 1) * 2; //patlak sıkışık dizi dışında çalışmıyor
-  //temp değerini indisbelirle fonksiyonunu çağırdıktan sonra arttır
-  for (let index2 = temp; index2 < tmpArray.length; index2++) {
-    //push gerçekleştir
-    subArrayFilteredIndex.push(tmpSubArray.toString());
-
-    console.log("maxSUM=" + maxSubArraySum);
-    console.log(tmpSubArray);
-    console.log("maxIndis=" + maxSubArrayIndis);
-
-    //subArray index dizisine at
-    checkSumIndex(tmpSubArray, tmpArray);
-    tmpSubArray[tmpSubArray.length - 1]++; //subArray son elemanı bir arttırarak gez.
+//kuyruk kontrol edilir
+function checkSubArrayEnd() {
+  var state;
+  for (
+    let index = subArray.length - 1, index2 = array.length - 1; //sondan dolaşarak doğruluk kontrol edilir
+    index >= 0;
+    index--, index2 -= 2
+  ) {
+    if (subArray[index] == index2) {
+      state = true;
+    } else {
+      state = false;
+      break;
+    }
   }
-  return tmpSubArray;
+  return state;
+}
+//realEnd if [15,17,19] shift işlemlerinden sonra
+//[0,2,4]'e gelmeli bu durumda 0. eleman shift yapılabilir
+
+function readyForShift() {
+  //sondan bir önceden birer birer geri bakarak sıkışık dizi olup olmadığı kontrol edilmeli
+  //eğer sıkışık dizi durumu yok ise sondan bir önceki terim örneğin 17. indis(gelebileceği son indis)
+  //durumuna gelmişse bir önceki yani sondan 2 öncekine bakılır ve 15. indiste olmaması durumunda
+  //o indis bir arttırılır eğer 15. indiste ise ondan bir öncekine bakılır bu şekilde devam eder
+  //arttırma sırasında sıkışıklık önemli bir faktör
+  //eğer forScroll değişkeni doğru şekilde arttırılmaz ise sonuç yanlış çıkacaktır.
+  //5 elemanlı bir dizinin 4,5. elemanları sona ulaştıysa 3. elemanı arttırmak için sıkışıklık sağlanmalı
+  //bu yüzden forScroll oraya kadar çok yükselmişken tekrardan hesap yapılmalı
+  //bu hesaba yalnızca bir eklenmeli
+
+  for (
+    let index = tmpArrayChanged.length - 1, index2 = subArray.length - 1; //son elemanlar göz ardı edilir
+    index2 >= 0; //son indise kadar dönülür
+    index -= 2, index2--
+  ) {
+    //son elemana da bakılmayacak -2 index2
+    if (index != subArray[index2]) {
+      indisBelirle(index2);
+      break;
+    }
+  }
 }
 
 /*
