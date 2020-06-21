e01964c57fcffb260df585403629ce0386b265ec
ex-pec <65666637+ex-pec@users.noreply.github.com>
2020-06-22 01:45:26 +0300

    end

--- try2.js
+++ try2.js
@@ -1,3 +1,36 @@
-function fonk2(params) {
-    
+//Soru 2: a'den b'ye kadar olan sayılardan, x'e tam bölünenlerin toplamını veren bir fonksiyon yazınız.
+var allResult;
+
+function fonk2() {
+  var a = 101;
+  var b = 150;
+  var x = 10;
+  
+  var state = true;
+  document.getElementById("add5").innerHTML =
+    a + " ile " + b + " arasında " + x + "'e bölünenler========>";
+
+  for (let index = 0, index2 = a; index < x; index++, index2++) {
+    if (index2 % x == 0) {
+      a = index2;
+      state = true;
+      break;
+    } else {
+      state = false;
+    }
+  }
+  var veri = "";
+  while (true) {
+    if (a + x <= b) {
+      veri += "<br>" + a;
+      a += x;
+    } else {
+      veri += "<br>" + a;
+      break;
+    }
+  }
+  //var veri = state ? divide(a, b, x) : "Hiç bölünen sayı yok";
+
+  var str = "Bölünen sayılar";
+  document.getElementById("add4").innerHTML = str + "<br>" + veri;
 }
